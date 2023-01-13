3ae8eb1c1716717d73d68a12010c866f2faedaf1
Juan Lobo <juanlobo11@outlook.com>
2023-01-13 14:44:59 -0400

    ..... campo faltante en el enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,7 @@ export const environment = {
   // URLApi: 'https://api.thomas-talk.me/',
   urlDBFULL: 'https://dbfull.thomas-talk.me/api',
   urlApisslThomas: 'https://apissl.thomas-talk.me/SAE',
+  urlConsultassslThomas: 'https://apissl.thomas-talk.me/Consultas',
   urlApisslThomasSend: 'https://apissl.thomas-talk.me/Envio',
   Token: 's1e1/c1r2e-2t.2a3r*3i4a',
   URLApi2: 'http://localhost:8888/',
@@ -18,19 +19,19 @@ export const environment = {
   UrlSubscriptions2: "ws://localhost:4300/subscriptions",
   upload_preset: "itenw8im",
   ApiKey: "167871278349249",
-  CloudName:"thomasv9",
+  CloudName: "thomasv9",
   tokendbFull: '81N2vjsIqq39qjGoEmDmMtjLqW7gLDA7vBV-Ffwuhwf-evejDaRGMrdSASny480GVOl7fcYfh21xfcpJWZ8VzQBHf0chPGOhyo9w3zJQ8OXEYGxwzxCU1gDplt3ebE4wDCkoujh4284bTkzz52AbNudtcR1HBq5_xU3mL5IJ4pqbeiFOJVa9',
   securityEncrt: 'T0rNaDoK4tr1Na?RTgcNmhKU=',
   ApiKeyApissl: '4p1_[51t3m4s]_f186@th0m4s',
   ApiSecret: "7I5iUVfNCJlWI8zS6E1vUSXM4oI",
-  urlPHPapi : 'http://127.0.0.1:8000/',
+  urlPHPapi: 'http://127.0.0.1:8000/',
   PHPUpload: false,
-  ApiMercantil:'https://backendmercantil.thomas-talk.me/', //'http://localhost:8090/' https://backendmercantil.thomas-talk.me/
-  TokenApiMercantil:'FCP*F4--3fGGPOK474512RGG**',
+  ApiMercantil: 'https://backendmercantil.thomas-talk.me/', //'http://localhost:8090/' https://backendmercantil.thomas-talk.me/
+  TokenApiMercantil: 'FCP*F4--3fGGPOK474512RGG**',
   KeyEncrypt: 'AstJuTnDhyUKyRee',
   WidgetID: 'BB611338173A55DC52494CF79D86A6CD',
   DireccionWidget: '14iE4zL57wjWtdnABwi5uiw8c73L9er597',
-  ApiTokenWidget :'CD6D80FC1E4168AAB045C3A80BD0B410',
+  ApiTokenWidget: 'CD6D80FC1E4168AAB045C3A80BD0B410',
   CaptchaSiteKey: '10000000-ffff-ffff-ffff-000000000001'
 };
 
