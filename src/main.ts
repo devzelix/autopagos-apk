8147fef5a89d4187e7f9aa70cfcf519ee296b12e
macardenas <michael.torrealba09@gmail.com>
2023-09-05 17:20:55 -0400

    ajustes saldo por abonado

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// if (environment.production) {
-//   enableProdMode();
-//   console.log = function() {}
-//   console.error =  function() {}
-//   console.warn = function() {} 
-// }
+if (environment.production) {
+  enableProdMode();
+  console.log = function() {}
+  console.error =  function() {}
+  console.warn = function() {} 
+}
 
 // // CANCELA TODOS LOS MENSAJES DE LA CONSOLA
-console.log = function() {}
-console.error =  function() {}
-console.warn = function() {}
+// console.log = function() {}
+// console.error =  function() {}
+// console.warn = function() {}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
