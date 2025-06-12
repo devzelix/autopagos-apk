def71d6eead40a59bf07af762b28e13de73755b4
Miguelrodac <rodacmig19@gmail.com>
2025-06-12 13:06:51 -0400

    Updates, and resolve bugs, add loaders and test with anulation payments

--- src/app/services/vposuniversal/vposuniversal-request.service.ts
+++ src/app/services/vposuniversal/vposuniversal-request.service.ts
@@ -101,7 +101,7 @@ export class VposuniversalRequestService {
           subscriberNum: _subscriber,
           mac_address: _register,
           response_code: res.data?.datavpos?.codRespuesta ?? 'response code undefined',
-          response_message: res.data?.datavpos?.mensaje ?? 'response message undefined',
+          response_message: res.data?.datavpos?.mensajeRespuesta ?? 'response message undefined',
           url_api: `${environment.API_URL_VPOS}/metodo/request/cardpay`,
           'is_success': isSuccess
         };
