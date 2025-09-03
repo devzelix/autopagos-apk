3d83b8dee4bd9b2c551dea3f624d71f6ac1b2a92
Miguelrodac <rodacmig19@gmail.com>
2025-09-03 15:16:55 -0400

    android version in proccess

---
+++ android/app/src/main/java/com/fibextelecom/autopago/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fibextelecom.autopago;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fibextelecom.autopago',
+  appName: 'AutoPago Fibex Telecom',
+  webDir: 'dist/form-pay-fibex',
+  // server: {
+  //   androidScheme: 'https',
+  // },
+};
+
+export default config;
